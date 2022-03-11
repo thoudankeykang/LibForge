462700f99d18f19b35e989568e47c131d7835301
LlysiX <smyler02@gmail.com>
2022-03-10 18:23:54 -0800

    GUI con2vr (WIP)
    janky but it works for now

--- LibForge/ForgeToolGUI/Inspectors/StartupInspector.cs
+++ LibForge/ForgeToolGUI/Inspectors/StartupInspector.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GameArchives;
+using GameArchives.STFS;
+using DtxCS;
+using DtxCS.DataTypes;
+using LibForge;
+using LibForge.Ark;
+using LibForge.CSV;
+using LibForge.Lipsync;
+using LibForge.Mesh;
+using LibForge.Midi;
+using LibForge.Milo;
+using LibForge.RBSong;
+using LibForge.SongData;
+using LibForge.Texture;
+using LibForge.Util;
 
 namespace ForgeToolGUI.Inspectors
 {
@@ -35,6 +51,133 @@ namespace ForgeToolGUI.Inspectors
     private void openFolderButton_Click(object sender, EventArgs e)
     {
       fb.openFolder_Click(sender, e);
+    }
+
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+      var ocd = new OpenFileDialog();
+      if (ocd.ShowDialog(this) == DialogResult.OK)
+        {
+          FolderBrowserDialog opd = new FolderBrowserDialog();
+          if (opd.ShowDialog(this) == DialogResult.OK)
+          {
+          var rbvrPath = opd.SelectedPath;
+          var conFile = STFSPackage.OpenFile(Util.LocalFile(ocd.FileName));
+          var tempDir = Path.Combine(Path.GetTempPath(), "forgetool_tmp_build");
+          var songs = PkgCreator.ConvertDLCPackage(conFile.RootDirectory.GetDirectory("songs"), false);
+          var entitlementNames = new List<string>();
+          foreach (DLCSong song in songs)
+          {
+            Directory.CreateDirectory(tempDir);
+            var shortname = song.SongData.Shortname;
+            var filePrefix = $"pc/songs_download/{shortname}/{shortname}";
+            var arks = new List<List<Tuple<string, IFile, int>>>();
+            arks.Add(new List<Tuple<string, IFile, int>>());
+
+            // convert mogg.dta to dta_pc
+            using (FileStream fs = File.OpenWrite(Path.Combine(tempDir, $"{shortname}.mogg.dta_dta_pc")))
+              DTX.ToDtb(song.MoggDta, fs, 3, false);
+            arks[0].Add(Tuple.Create($"{filePrefix}.mogg.dta_dta_pc", Util.LocalFile($"{tempDir}/{shortname}.mogg.dta_dta_pc"), -1));
+            // convert moggsong to dta_pc
+            using (FileStream fs = File.OpenWrite(Path.Combine(tempDir, $"{shortname}.moggsong_dta_pc")))
+              DTX.ToDtb(song.MoggSong, fs, 3, false);
+            arks[0].Add(Tuple.Create($"{filePrefix}.moggsong_dta_pc", Util.LocalFile($"{tempDir}/{shortname}.moggsong_dta_pc"), -1));
+            // add empty vrevents to rbmid
+            song.RBMidi.Format = RBMid.FORMAT_RBVR;
+            song.RBMidi.VREvents = new RBMid.RBVREVENTS();
+            using (var rbmid = File.OpenWrite(Path.Combine(tempDir, $"{shortname}.rbmid_pc")))
+              RBMidWriter.WriteStream(song.RBMidi, rbmid);
+            arks[0].Add(Tuple.Create($"{filePrefix}.rbmid_pc", Util.LocalFile($"{tempDir}/{shortname}.rbmid_pc"), -1));
+            // add rbsong
+            using (var rbsongFile = File.OpenWrite(Path.Combine(tempDir, $"{shortname}.rbsong")))
+              new RBSongResourceWriter(rbsongFile).WriteStream(song.RBSong);
+            arks[0].Add(Tuple.Create($"{filePrefix}.rbsong", Util.LocalFile($"{tempDir}/{shortname}.rbsong"), -1));
+            // add songdta
+            song.SongData.KeysRank = 0;
+            song.SongData.RealKeysRank = 0;
+            using (var songdtaFile = File.OpenWrite(Path.Combine(tempDir, $"{shortname}.songdta_pc")))
+              SongDataWriter.WriteStream(song.SongData, songdtaFile);
+            arks[0].Add(Tuple.Create($"{filePrefix}.songdta_pc", Util.LocalFile($"{tempDir}/{shortname}.songdta_pc"), -1));
+            // add album art (incompatible)
+            if (song.SongData.AlbumArt)
+            {
+              using (var artFile = File.OpenWrite(Path.Combine(tempDir, $"{shortname}.png_pc")))
+                TextureWriter.WriteStream(song.Artwork, artFile);
+              arks[0].Add(Tuple.Create($"{filePrefix}.png_pc", Util.LocalFile($"{tempDir}/{shortname}.png_pc"), -1));
+            }
+            // add mogg
+            arks[0].Add(Tuple.Create($"{filePrefix}.mogg", song.Mogg, -1));
+
+            var builder = new ArkBuilder($"{shortname}_pc", arks);
+            Console.WriteLine($"Writing {shortname}_pc.hdr and ARK files to {rbvrPath}...");
+            builder.Save(rbvrPath, 0x0);
+            entitlementNames.Add($"DLC = song_{shortname}");
+            Directory.Delete(tempDir, true);
+          }
+          Console.WriteLine("Conversion complete! Add the following DLC SKUs to your entitlement list:");
+          foreach (string sku in entitlementNames)
+          {
+            File.WriteAllLines(rbvrPath + "/SKUs.txt", entitlementNames);
+            MessageBox.Show("Conversion Complete! Add the DLC SKUs in SKUs.txt to GammonConfig.ini");
+            Console.WriteLine(sku);
+          }
+        }
+        }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      var ocd = new OpenFileDialog();
+      if (ocd.ShowDialog(this) == DialogResult.OK)
+      {
+        FolderBrowserDialog opd = new FolderBrowserDialog();
+        if (opd.ShowDialog(this) == DialogResult.OK)
+        {
+          var rbvrPath = opd.SelectedPath;
+          var conFile = STFSPackage.OpenFile(Util.LocalFile(ocd.FileName));
+          var tempDir = Path.Combine(Path.GetTempPath(), "forgetool_tmp_build");
+          var songs = PkgCreator.ConvertDLCPackage(conFile.RootDirectory.GetDirectory("songs"), false);
+          var entitlementNames = new List<string>();
+          foreach (DLCSong song in songs)
+          {
+            var shortname = song.SongData.Shortname;
+            var songPath = Path.Combine(rbvrPath, "songs", "pc", "songs", shortname);
+            Directory.CreateDirectory(songPath);
+            // add mogg
+            using (var mogg = File.OpenWrite(Path.Combine(songPath, $"{shortname}.mogg")))
+            using (var conMogg = song.Mogg.GetStream())
+            {
+              conMogg.CopyTo(mogg);
+            }
+            // convert mogg.dta to dta_pc
+            using (FileStream fs = File.OpenWrite(Path.Combine(songPath, $"{shortname}.mogg.dta_dta_pc")))
+              DTX.ToDtb(song.MoggDta, fs, 3, false);
+            // convert moggsong to dta_pc
+            using (FileStream fs = File.OpenWrite(Path.Combine(songPath, $"{shortname}.moggsong_dta_pc")))
+              DTX.ToDtb(song.MoggSong, fs, 3, false);
+            // add empty vrevents to rbmid
+            song.RBMidi.Format = RBMid.FORMAT_RBVR;
+            song.RBMidi.VREvents = new RBMid.RBVREVENTS();
+            using (var rbmid = File.OpenWrite(Path.Combine(songPath, $"{shortname}.rbmid_pc")))
+              RBMidWriter.WriteStream(song.RBMidi, rbmid);
+            // add rbsong
+            using (var rbsongFile = File.OpenWrite(Path.Combine(songPath, $"{shortname}.rbsong")))
+              new RBSongResourceWriter(rbsongFile).WriteStream(song.RBSong);
+            // add songdta
+            song.SongData.KeysRank = 0;
+            song.SongData.RealKeysRank = 0;
+            using (var songdtaFile = File.OpenWrite(Path.Combine(songPath, $"{shortname}.songdta_pc")))
+              SongDataWriter.WriteStream(song.SongData, songdtaFile);
+            // add album art
+            if (song.SongData.AlbumArt)
+            {
+              using (var artFile = File.OpenWrite(Path.Combine(songPath, $"{shortname}.png_pc")))
+                TextureWriter.WriteStream(song.Artwork, artFile);
+            }
+            MessageBox.Show("Conversion complete! Add the songs folder to your main RBVR ark using patchcreator in arkhelper");
+          }
+        }
+      }
     }
   }
 }
